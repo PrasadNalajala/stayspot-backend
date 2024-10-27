b406d0446f242d55e0c635275b5011de07268c99
Prasad <nalajalaprasad1018@gmail.com>
2024-10-27 23:10:52 +0530

    added jwt token

--- api/index.js
+++ api/index.js
@@ -84,7 +84,8 @@ app.post('/register', async (req, res) => {
                 if (err) {
                     return res.status(500).json({ error: err.message });
                 }
-                res.status(201).json({ id: result.insertId, name, email });
+                const token = jwt.sign({ id: result.insertId, email }, process.env.JWT_SECRET, { expiresIn: '30d' });
+                res.status(201).json({ id: result.insertId, name, email, token });
             });
         });
     } catch (error) {
@@ -111,10 +112,7 @@ app.post('/login', (req, res) => {
         if (!match) {
             return res.status(401).json({ error: 'Invalid password' });
         }
-
-        // const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        res.status(200).json({ message: 'Login successful'});
+        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '30d' });
+        res.status(200).json({ message: 'Login successful', token });
     });
 });
-
