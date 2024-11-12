c05aa63dd1e3d70dec50732e238df98d4f12de1b
Prasad <nalajalaprasad1018@gmail.com>
2024-11-12 23:52:47 +0530

    changed port

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
